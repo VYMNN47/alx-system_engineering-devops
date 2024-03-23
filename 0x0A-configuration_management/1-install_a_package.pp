ef2c95e1645ea1b998e82168366a097abec05176
aymnn <ayman.belahsen.4@gmail.com>
2024-03-23 15:56:17 +0000

    .

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-# install flask from pip3
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+#!/usr/bin/pup
+#Installs flask from pip3 using Puppet
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package { 'Werkzeug':
+  ensure   => '2.0.*',
+  provider => 'pip3'
+}
