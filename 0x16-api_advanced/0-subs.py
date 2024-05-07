69a9fb7e064e1c259ed44b922250c44aa021c909
aymnn <ayman.belahsen.4@gmail.com>
2024-05-07 18:40:41 +0100

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,10 +12,8 @@ def number_of_subscribers(subreddit):
     Function that queries the Reddit API
     if not a valid subreddit, return 0.
     """
-    req = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "Custom"},
-    )
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    req = requests.get(url, headers={"User-Agent": "Custom"},)
 
     if req.status_code == 200:
         return req.json().get("data").get("subscribers")
