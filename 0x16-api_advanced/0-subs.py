06d5a8782c4d1f566d5d6ebdc12f6277bf1eb742
aymnn <ayman.belahsen.4@gmail.com>
2024-05-07 18:35:47 +0100

    .

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,13 +8,14 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-"""
-function that queries the Reddit API and
-returns the number of subscribers
-if not a valid subreddit, return 0.
-"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    req = requests.get(url, headers={"User-Agent": "Custom"})
+    """
+    Function that queries the Reddit API
+    if not a valid subreddit, return 0.
+    """
+    req = requests.get(
+        "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+    )
 
     if req.status_code == 200:
         return req.json().get("data").get("subscribers")
